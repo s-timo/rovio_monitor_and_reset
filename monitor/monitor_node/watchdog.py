38fa531018ce0916ae9c68becfc6493e0c110276
Reto Bischoff <retob@student.ethz.ch>
2016-06-20 11:22:30 +0200

    Kommetare in Programm nun  auf Englisch

--- monitor/monitor_node/watchdog.py
+++ monitor/monitor_node/watchdog.py
@@ -22,7 +22,7 @@ def odometryCb(msg):
    	#time=rospy.Time.now()
  	#print rospy.Time.now()-time
 	
- 	#globale Variabeln verfuegbar mache
+ 	#make global variables available
 	global lastdelta
  	global zwpose
  	global maxspeed
@@ -33,25 +33,25 @@ def odometryCb(msg):
  	global initalizing
 
  	if initalizing:
- 		initialoffset[i]= rospy.get_time()-msg.header.stamp.to_sec() #initialisierung des offset zwischen Headertimestamp und aktuel
+ 		initialoffset[i]= rospy.get_time()-msg.header.stamp.to_sec() #initializing the offset between Headertimestamp and now
  		if i==19:
  			initialoffset.sort()
- 			offset=initialoffset[10] #Median aller gemessenen Offfsets
+ 			offset=initialoffset[10] #median of all meassured offsets
  			lastdelta=rospy.get_time()-msg.header.stamp.to_sec()- offset
  			initalizing=False
  			#print "Initalzing of Monitor finished"
 
  	else:
-	 	deltatime=rospy.get_time()-msg.header.stamp.to_sec()- offset #Zeit zwischen aktueller zeit und header Timestamp
+	 	deltatime=rospy.get_time()-msg.header.stamp.to_sec()- offset #time between header timestamp and now
 	 
-	 	deriv=deltatime-lastdelta #Zeit in s mit der der Delay ansteigt
+	 	deriv=deltatime-lastdelta #time in [s] with which the delay increase
 	 	
 	 	print deltatime 
 	 	#print deriv
-	 	if deriv>0.015: # default. 0.025 ausloeseargument, wenn delay zu schnell steigt
+	 	if deriv>0.015: # default. 0.025 trigger, if delay increase to fast
 	 		hysterese=hysterese+1
 	 		
-	 	#elif deltatime>0.05 :# ausloeseargument, wenn delay zu gross ist, muss zuerst in live probiert werden ob dort dely auch dauernd zunimmt
+	 	#elif deltatime>0.05 :# trigger, if delay is too bis muss zuerst in live probiert werden ob dort dely auch dauernd zunimmt
 	 	#	hysterese=hysterese+1
 	 	else:
 	 		if hysterese>0:
@@ -59,11 +59,11 @@ def odometryCb(msg):
 	 		else:
 	 			hysterese=0
 	 	if hysterese > 2: #min 3 mal muss Indikator anspringen bis ausgeloest wird, hysteresemethode muss angepasst werden
-	 		reset("delay",zwpose[i])#reset Aufruf, mit Wert von vor 20 mal
+	 		reset("delay",zwpose[i])#reset call, with value before 20 times (1 second)
 	 		hysterese=0
 
-	 	lastdelta=deltatime #alte Delaytime global speichern
-		#lastmsg=msg.header.stamp.to_sec() #alte Headertime speichern. Abstand zwischen Msgs sind aber randowm daher nicht in gebrauch
+	 	lastdelta=deltatime #save old delay value
+		#lastmsg=msg.header.stamp.to_sec() #save old headertime. Spacing between the messages are randomly distributed, that's why this mehtode isn't in use
 
 		#Auslesen des Aktuellen absoluten speed
 		speed= msg.twist.twist.linear.x*msg.twist.twist.linear.x+msg.twist.twist.linear.y*msg.twist.twist.linear.y+msg.twist.twist.linear.z*msg.twist.twist.linear.z
@@ -71,35 +71,35 @@ def odometryCb(msg):
 		#nice to know
 		#if speed>maxspeed:
 		#  	maxspeed=speed
-		if speed> 2.25:#default: 2.25=1.5 im Qudrat um Comptime zu sparen (Wurzelziehen)
-			reset("speed",zwpose[i])#reset Aufruf, mit Wert von vor 20 mal
+		if speed> 2.25:#default: 2.25=1.5 squarde. To safe computational time
+			reset("speed",zwpose[i])#reset call with value before 20 times 81 second)
 
 	#verwaltung
-	zwpose[i]=msg.pose #aktuelle Pose in Array schreiben
+	zwpose[i]=msg.pose #save actual pose
 		
 	i=(i+1)%20 #0-19 usw.
 
    		
-def reset(reason, pose):#Reset funktion Grund, Pose to reset
+def reset(reason, pose):#Reset function reason, Pose to reset
 	global initalizing
 
 	print "Rovio reseted due to: "+reason
 	rospy.wait_for_service('/rovio/reset_to_pose')
-	rovioreset=rospy.ServiceProxy('/rovio/reset_to_pose',SrvResetToPose) #service call bereit machen
+	rovioreset=rospy.ServiceProxy('/rovio/reset_to_pose',SrvResetToPose) #arm service call 
 
 	newpose=geometry_msgs.msg.Pose()
 	newpose.position=pose.pose.position
-	newpose.orientation=pose.pose.orientation #Pose ohne covariance bereitstellen
-	response=rovioreset(newpose)#Servicecall ausloesen
-	initalizing=True# Monitor-Programm wieder zum initialisieren bringen
+	newpose.orientation=pose.pose.orientation #make Pose without covariance ready
+	response=rovioreset(newpose)#trigger servicecall 
+	initalizing=True# make monitor programm initializing again
 	#print "Rovio reseted, Monitor reinitializing"
 
 
-if __name__ == "__main__": #Einstiegspunkt
+if __name__ == "__main__": #entry
 
 	rospy.init_node('oodometry', anonymous=True) #make node 
 	
-	#initialisieren der globalen Variablen
+	#initializing the global variables
 	global lastdelta
 	global maxspeed
 	global initialoffset
@@ -118,7 +118,7 @@ if __name__ == "__main__": #Einstiegspunkt
 
 
 	print "Monitor V4.8 started"
-	#rovio listener definieren/laufen lassen
+	#rovio listener defining/start
 	savepose=rospy.Subscriber('rovio/odometry',Odometry,odometryCb)
 	rospy.spin()
 
